a16a5f15908ac16446363d08a17903faa46f1778
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-07-25 19:22:10 +0700

    Add relationships to MahasiswaDaftarUlang

--- app/MahasiswaDaftarUlang.php
+++ app/MahasiswaDaftarUlang.php
@@ -11,4 +11,19 @@ class MahasiswaDaftarUlang extends Model
     /* We use UUID */
     public $incrementing = false;
     use Uuids;
+
+    public function programStudi()
+    {
+    	return $this->belongsTo('App\ProgramStudi', 'program_studi', 'kode_program_studi');
+    }
+
+    public function jalurPenerimaan()
+    {
+    	return $this->belongsTo('App\JalurPenerimaan', 'jalur_penerimaan');
+    }
+
+    public function agamaDetail()
+    {
+    	return $this->belongsTo('App\Agama', 'agama');
+    }
 }
